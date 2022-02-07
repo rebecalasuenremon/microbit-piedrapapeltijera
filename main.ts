a9b7ca472069ec5d1fc7415f2a2be5375a36e3ea
rebecalasuenremon <98742881+rebecalasuenremon@users.noreply.github.com>
2022-02-07 11:49:02 +0100

    Holiiii

--- main.ts
+++ main.ts
@@ -1,3 +1,29 @@
-basic.forever(function () {
-	
+let Jugada = 0
+input.onGesture(Gesture.Shake, function () {
+    Jugada = randint(1, 3)
+    if (Jugada == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (Jugada == 2) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            # # . # #
+            # # . # #
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    }
 })
